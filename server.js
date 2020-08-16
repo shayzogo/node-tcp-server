dfd39bf8595c4afb6a0661d27cb2cbaae01b647e
Shay Zeevi <shay@zogomarket.com>
2020-08-16 11:45:22 +0300

    added example code for working with cluster if serjo will choose to work with

--- server.js
+++ server.js
@@ -10,7 +10,43 @@ const adminTransporter = require('./tcpModules/AdminTransportModule');
 const dbTransporter = require('./tcpModules/DBTransportModule');
 const proxyTransporter = require('./tcpModules/ProxyTransportModule');
 
+server.listen(port, host, () => {
+   console.log('TCP Server is running on port ' + port + '.');
+});
 
+let sockets = [];
+
+server.on('connection', function (sock) { // connection event
+   console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+   sockets.push(sock);
+   sock.on('data', function (data) { // when receiving data
+      let messageData = JSON.parse(`${data}`); // we master make it a string so we want get buffer
+      const drivers = JSON.parse(messageData.drivers);
+      drivers.forEach(driver => {
+         switch (driver) {
+            case 'admin':
+               adminTransporter.AdminTransporterModule(messageData);
+               break;
+            case 'db':
+               dbTransporter.DBTransportModules(messageData);
+               break;
+            case 'proxy':
+               proxyTransporter.ProxyTransportModule(messageData);
+               break;
+         }
+      });
+   });
+
+   sock.on('close', function (data) {
+      let index = sockets.findIndex(function (o) {
+         return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+      })
+      if (index !== -1) sockets.splice(index, 1);
+      console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+   });
+});
+
+/*
 if (cluster.isMaster) {
    for (var i = 0; i < cpus; i++) {
       cluster.fork();
@@ -58,6 +94,7 @@ if (cluster.isMaster) {
       });
    });
 }
+*/
 
 /*
 server.listen(port, host, () => {

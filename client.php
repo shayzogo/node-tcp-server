d5ddc426fc0427abb9e9ca3c0ad7d9bf427dd5fb
Shay Zeevi <shay@zogomarket.com>
2020-08-16 10:37:29 +0300

    swe will stop here so maybe we do not need redis writing

--- client.php
+++ client.php
@@ -4,7 +4,7 @@ $options = new stdClass();
 $options->logString = 'This is a test message';
 $options->logType = 'deposit';
 $options->site = 'ares';
-$options->pushDrivers = ['email', 'db', 'socket'];
+$options->pushDrivers = ['db'];
 $pusher = new Pusher($options);
 $pusher->pushLogToServer();
 

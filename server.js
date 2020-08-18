b73daff8ec3d39f85a64358f416f844042da8ec1
Shay Zeevi <shay@zogomarket.com>
2020-08-18 13:45:49 +0300

    making more documentations and clearing cluster example, no need

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ server.on('connection', function (sock) { // connection event
    console.log('TCP listening on: ' + sock.remoteAddress + ':' + sock.remotePort);
    sockets.push(sock);
    sock.on('data', function (data) { // listen for incoming data
-      const messageData = JSON.parse(`${data}`);
+      const messageData = JSON.parse(`${data}`); // The data is buffered so we need to string it and parse
       const transporters = typesMapper.types[messageData.type];
       transporters.forEach(transporter => {
-         const transporterName = transporter + 'Queue';
+         const transporterName = transporter + 'Queue'; // Build the bull queue name for this transporter
          try {
             eval(transporterName).add({msg: data + ''}); // use to push the message to bull queue
          } catch (e) {
@@ -52,53 +52,3 @@ server.on('connection', function (sock) { // connection event
       console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
    });
 });
-
-/*
-if (cluster.isMaster) {
-   for (var i = 0; i < cpus; i++) {
-      cluster.fork();
-   }
-} else {
-   server.listen(port, host, () => {
-      console.log('TCP Server is running on port ' + port + '.');
-   });
-
-   let sockets = [];
-
-   server.on('connection', function (sock) { // connection event
-      console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-      sockets.push(sock);
-      sock.on('data', function (data) { // when receiving data
-
-         let messageData = data + '';
-         try {
-            const parsedMessage = JSON.parse(messageData);
-            const drivers = JSON.parse(parsedMessage.drivers);
-            drivers.forEach(driver => {
-               switch (driver) {
-                  case 'admin':
-                     adminTransporter.AdminTransporterModule(messageData);
-                     break;
-                  case 'db':
-                     dbTransporter.DBTransportModules(messageData);
-                     break;
-                  case 'proxy':
-                     proxyTransporter.ProxyTransportModule(messageData);
-                     break;
-               }
-            });
-         } catch (e) {
-            console.log(e);
-         }
-      });
-
-      sock.on('close', function (data) {
-         let index = sockets.findIndex(function (o) {
-            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
-         })
-         if (index !== -1) sockets.splice(index, 1);
-         console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-      });
-   });
-}
-*/

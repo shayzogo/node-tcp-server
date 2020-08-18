d33567c705abb7b41dca5465230b545b9c85a1be
Shay Zeevi <shay@zogomarket.com>
2020-08-18 13:08:16 +0300

    writing some documentation on the server

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const net = require('net');
-const port = 7070;
-const host = '127.0.0.1';
 const server = net.createServer();
+const host = '127.0.0.1';
+const port = 7070;
+
+/**
+ * Making instances of the bull queue objects to use inside the server when needed,
+ * It looks like its not in use but it is with `eval(transporterName).add({msg: data + ''})`;
+ * @type {Queue}
+ */
 const Queue = require('bull');
-const typesMapper = require('./logTypeMapper.json');
 const dbQueue = new Queue('dbQueue');
 const adminQueue = new Queue('adminQueue');
 const groupReportQueue = new Queue('groupReportQueue');
 
+/**
+ * There are currently only 3 gateways, inside this json you can map any log type to any gateway
+ * please make changes only to this file and dont do any bypasses
+ * @type {{types: {affiliate: array[string], deposit: array[string], game: array[string]}}}
+ */
+const typesMapper = require('./logTypeMapper.json');
+
+/**
+ * Open listener on host+port
+ */
 server.listen(port, host, () => {
    console.log('TCP Server is running on port ' + port + '.');
 });
 
 let sockets = [];
-
 server.on('connection', function (sock) { // connection event
    console.log('TCP listening on: ' + sock.remoteAddress + ':' + sock.remotePort);
    sockets.push(sock);
@@ -23,15 +37,14 @@ server.on('connection', function (sock) { // connection event
       transporters.forEach(transporter => {
          const transporterName = transporter + 'Queue';
          try {
-            eval(transporterName).add({msg: data + ''});
+            eval(transporterName).add({msg: data + ''}); // use to push the message to bull queue
          } catch (e) {
             console.log(e);
          }
       });
-
    });
 
-   sock.on('close', function (data) {
+   sock.on('close', function (data) { // on close connection event
       let index = sockets.findIndex(function (o) {
          return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
       })

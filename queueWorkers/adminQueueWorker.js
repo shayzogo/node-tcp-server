6efd58cd30a558b14a298d6d88c7ac81495c0067
Shay Zeevi <shay@zogomarket.com>
2020-08-23 13:33:09 +0300

    connection transporters to workers

--- queueWorkers/adminQueueWorker.js
+++ queueWorkers/adminQueueWorker.js
@@ -1,6 +1,8 @@
 const Queue = require('bull');
+const AdminTransporter = require('./../classes/AdminTransporter');
 
 const adminQueue = new Queue('adminQueue', {redis: {port: 6379, host: '127.0.0.1', password: ''}});
 adminQueue.process(async (work) => {
-   console.log(work.data);
+   const adminTransporter = new AdminTransporter(work.data);
+   adminTransporter.sendToAdmins();
 }, 50);
--- queueWorkers/groupReportQueueWorker.js
+++ queueWorkers/groupReportQueueWorker.js
@@ -1,6 +1,8 @@
 const Queue = require('bull');
-
+const GroupReportTransporter = require('./../classes/GroupReportTransporter');
 const groupReportQueue = new Queue('dbQueue', {redis: {port: 6379, host: '127.0.0.1', password: ''}});
+
 groupReportQueue.process(async (work) => {
-   console.log(work.data);
+   const groupRequestTransporter = new GroupReportTransporter(work.data);
+   groupRequestTransporter.sendMessageToGroupReports();
 }, 100);
